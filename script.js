0a056026c144c54e3077cfd0c91d5509afee2d30
dewittd14 <deanna.dewitt14@gmail.com>
2022-08-16 21:04:01 -0400

    changed const into a var for requirements

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ function generatePassword() {
   //1. Prompt the user for the password criteria
   //     a. Password Length 8 < 128
   //     b. lowercase, uppercase, numbers, special characters
-  const numbers = [1,2,3,4,5,6,7,8,9,0];
-  const symboles = ["@", "#", "$", "&"]
-  const characterCodes = Array.from(Array(26).map( (_, i)=> i + 97);
-  const lowercaseLetters = characterCodes.map(code => String.fromCharCode(code));
-  const uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase())
+  var characterLength = 8;
+  var choiceArr = [];
+
+  var specialCharacterArr = ['!', '@', '#', '$', '%', '&', '*', '-', '+', '=', '?',];
+  var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+  var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+  var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
   //2. Validate the input
   //3. generate password based on criteria. 
 
