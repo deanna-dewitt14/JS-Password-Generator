67b77a6214596c6b00b6c5b5c487a515642f09e5
dewittd14 <deanna.dewitt14@gmail.com>
2022-08-16 21:34:33 -0400

    added if statemenets to getPrompts function

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function getPrompts(){
-  characterLength = prompt ("How many characters do you want your password to be? (8 - 128 characters"))
-}
- 
-function generatePassword() {
+function generatePassword() {   
   console.log("hey! you clicked the button")
 
   //1. Prompt the user for the password criteria
@@ -22,9 +18,29 @@ function generatePassword() {
   //2. Validate the input
   //3. generate password based on criteria. 
 
-
-  //4. Display generated password on the page. 
+//4. Display generated password on the page. 
   return "Generated password will go here!";
+}
+function getPrompts(){
+  characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters")); //NaN
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) { //hoping this will all be false
+    alert("Character length has to be a number, 8 - 128 digits. Please try again.");
+    return false;
+  }
+  if (confirm("Would you like lowercase letters in your passwords?")) {
+    choiceArr = choiceArr.concat(lowerCaseArr);
+  }
+  if (confirm("Would you like uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCaseArr);
+  }
+  if (confirm("Would you like numbers in your password?")) {
+    choiceArr = choiceArr.concat(numberArr);
+  }
+  if (confirm("Would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharacterArr);
+  }
+  return true;
 }
 
 // Write password to the #password input
@@ -33,7 +49,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
