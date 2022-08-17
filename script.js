0db3ae784395b7c81f7cab3a836598c6ea5d4364
dewittd14 <deanna.dewitt14@gmail.com>
2022-08-16 21:07:40 -0400

    added getPrompts function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getPrompts(){
+  characterLength = prompt ("How many characters do you want your password to be? (8 - 128 characters"))
+}
+ 
 function generatePassword() {
   console.log("hey! you clicked the button")
 
@@ -14,7 +18,7 @@ function generatePassword() {
   var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
   var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
   var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
+ 
   //2. Validate the input
   //3. generate password based on criteria. 
 
