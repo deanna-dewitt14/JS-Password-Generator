2d959ef3aef5e258079be3b841097561e00d596b
dewittd14 <deanna.dewitt14@gmail.com>
2022-08-16 23:45:03 -0400

    rearranged code to have a clear formate

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+//1. Prompt the user for the password criteria
+//     a. Password Length 8 < 128
+//     b. lowercase, uppercase, numbers, special characters
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharacterArr = ['!', '@', '#', '$', '%', '&', '*', '-', '+', '=', '?',];
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var correctPrompts = getPrompts(); //returns true or false
+  var passwordText = document.querySelector("#password");
+
+  if (correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+}
 
 function generatePassword() {   
   console.log("hey! you clicked the button")
@@ -8,20 +35,9 @@ function generatePassword() {
       var randomIndex = Math.floor(Math.random() * choiceArr.length);
       password = password + choiceArr [randomIndex];
     }
-    return password; //4. Display generated password on the page. 
+    return password; //Display generated password on the page. 
   }
 
-  //1. Prompt the user for the password criteria
-  //     a. Password Length 8 < 128
-  //     b. lowercase, uppercase, numbers, special characters
-  var characterLength = 8;
-  var choiceArr = [];
-
-  var specialCharacterArr = ['!', '@', '#', '$', '%', '&', '*', '-', '+', '=', '?',];
-  var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
-  var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
-  var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
- 
   //2. Validate the input
   //3. generate password based on criteria. 
 
@@ -48,21 +64,3 @@ function getPrompts(){
   }
   return true;
 }
-
-// Write password to the #password input
-function writePassword() {
-  var correctPrompts = getPrompts(); //returns true or false
-  var passwordText = document.querySelector("#password");
-
-  if (correctPrompts) {
-    var newPassword = generatePassword();
-    passwordText.value = newPassword;
-  } else {
-    passwordText.value = "";
-  }
-}
-passwordText.value = password;
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
